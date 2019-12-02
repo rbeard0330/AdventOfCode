5aa6fdc4f004ce062006b7f4128e8dbbd8a537c9
Nate Meyvis <nate@blackhound.io>
2019-12-01 20:22:41 -0500

    bugfix, very slight improvements

--- nate/d1.py
+++ nate/d1.py
@@ -12,8 +12,8 @@ def fuel_extended(module):
         total += (temp := fuel(temp))
     return total
 
-assert fuel(100756) == 33583 # given
-assert (t2 := fuel_extended(100756)) == 50346, str(t2) # also given
+assert (fuel_required := fuel(100756)) == 33583, str(fuel_required) # given
+assert (fuel_required := fuel_extended(100756)) == 50346, str(fuel_required) # also given
 
-print(sum(map(fuel, module))) #This gives part 1's answer.
+print(sum(map(fuel, modules))) #This gives part 1's answer.
 print(sum(map(fuel_extended, modules))) #This gives part 2's answer.
