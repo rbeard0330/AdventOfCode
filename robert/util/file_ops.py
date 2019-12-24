51767812e10e98e95305c05a31862d7b83248496
Robert Beard <robert.a.beard@gmail.com>
2019-12-23 23:45:07 -0500

    d17

--- robert/util/file_ops.py
+++ robert/util/file_ops.py
@@ -3,5 +3,5 @@ import os.path
 
 def get_input_file_name(str):
     return os.path.join(
-            "c:\\Data\\Code\\Advent2019\\AdventofCode2019\\robert\\",
+            "c:\\Code\\AdventofCode2019\\robert\\",
             "inputs", str)
